47e91b53d2fe6551484065a08e04e32781094af3
vantonuks <vantonuks@inbox.lv>
2023-09-27 12:11:04 +0300

    initial project

---
+++ Controllers/AdminApiController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner.Controllers
+{
+    [Authorize]
+    [Route("admin-api")]
+    [ApiController]
+    public class AdminApiController : ControllerBase
+    {
+        [Route("flights/{id}")]
+        [HttpGet]
+        public IActionResult GetFlights(int id)
+        {
+            return NotFound();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+using FlightPlanner.Handlers;
+using Microsoft.AspNetCore.Authentication;
+
+namespace FlightPlanner
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddAuthentication("BasicAuthentication")
+                .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+            
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
